56f071c2cd1722ce87974280ca8f26b511a61f96
Ankitshetty2002 <71144744+Ankitshetty2002@users.noreply.github.com>
2020-11-19 21:13:06 +0530

    Add files via upload

---
+++ class and object.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class Sub
+
+{
+
+    public:
+
+     void Submission()
+
+     {
+
+         cout<<"\n YOUR ASSIGNMENT HAS BEEN SUBMITTED";
+
+     }
+
+};
+int main()
+
+{
+ 
+   Sub s;
+
+    s.Submission();
+
+    return 0;
+
+}    
