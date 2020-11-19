c9d1e5caeba9a16b4cf0989d35ecc8653f9f7053
Ankitshetty2002 <71144744+Ankitshetty2002@users.noreply.github.com>
2020-11-19 21:09:34 +0530

    Add files via upload

---
+++ class method.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+class Employee
+
+{
+
+    public:
+
+    string name;
+
+    string shift;
+
+    float salary;
+
+    void insert(string n,string h,float s)
+
+    {
+
+        name=n;
+
+        shift=h;
+
+        salary=s;
+
+    }
+
+    void display()
+
+    {
+
+        cout<<name<<" "<<shift<<" "<<salary<<"\n";
+
+    }
+
+};
+
+int main()
+
+{
+
+    Employee e1;
+
+    Employee e2;
+
+    Employee e3;
+
+    e1.insert("Manohar","day",35000);
+
+    e2.insert("Ramesh","day",40000);
+
+    e3.insert("Prasad","night",45000);
+
+    e1.display();
+
+    e2.display();
+
+    e3.display();
+
+    return 0;
+
+}
+
+
+    
