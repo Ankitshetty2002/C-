846dabba29c48c5d8425c9276a6237c5480d1128
Ankitshetty2002 <71144744+Ankitshetty2002@users.noreply.github.com>
2020-11-19 21:08:10 +0530

    Add files via upload

---
+++ object of class and acess class attributes.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std; 
+
+class Student
+ 
+{
+
+    public:
+
+    string name;
+
+    int rollno;
+ 
+    float per; 
+
+};
+ 
+int main()
+
+{
+
+    Student std;
+
+    std.name= "Ankit Pramod Shetty";
+
+    std.rollno= 44;
+
+    std.per= 8.76;
+
+    cout<<"\n Student's name:"<<std.name;
+
+    cout<<"\n Student's roll no:"<<std.rollno;
+
+    cout<<"\n Student's per:"<<std.per;
+
+    return 0;
+
+}
