01775cb806ad9d48e6c606fc40dcd83be5123d16
Ankitshetty2002 <71144744+Ankitshetty2002@users.noreply.github.com>
2020-11-21 20:13:10 +0530

    Add files via upload

---
+++ largest number.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+    float n1,n2,n3;
+
+    cout<<"Enter three numbers: ";
+
+    cin>>n1>>n2>>n3;
+
+    if(n1>n2)
+
+      {
+
+         if(n1>n3)
+
+          cout<<"\n Largest number is "<<n1;
+
+          else
+
+          cout<<"\n Largest number is "<<n3;
+
+      }
+
+      else
+
+      {
+
+          if(n2>n3)
+
+          cout<<"\n Largest number is "<<n2;
+
+          else
+
+          cout<<"\n Largest number is "<<n3;
+
+      }
+
+      return 0;
+
+}
