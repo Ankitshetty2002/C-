c4020f4359287c7358ad6d3f47721a532857b22e
Ankitshetty2002 <71144744+Ankitshetty2002@users.noreply.github.com>
2020-11-21 20:12:16 +0530

    Add files via upload

---
+++ even or odd.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+    int n;
+
+    cout<<"Enter the value: ";
+
+    cin>>n;
+
+    if(n%2==0)
+
+      cout<<"\n It is a even number";
+
+    else
+
+     cout<<"\n It is odd number";
+
+    return 0;
+
+}
