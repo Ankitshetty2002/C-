3887068e27ee05bf156c614971b3fe05f5d9266d
Ankitshetty2002 <71144744+Ankitshetty2002@users.noreply.github.com>
2020-11-21 20:10:01 +0530

    Add files via upload

---
+++ addition of two.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std; 
+
+int main()
+
+{
+
+    int x,y,z;
+
+    cout<<"Enter two numbers: ";
+
+    cin>>x>>y;
+
+    z=x+y;
+
+    cout<<"Sum of two given numbers is " << z;
+
+    return 0;
+
+}
