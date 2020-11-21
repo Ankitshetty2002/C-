e59373ba119e69cde95656c8c61d1aa98c801e02
Ankitshetty2002 <71144744+Ankitshetty2002@users.noreply.github.com>
2020-11-21 20:11:07 +0530

    Add files via upload

---
+++ swapping.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+    int n1,n2,s;
+
+    cout<<"Enter two values: ";
+
+    cin>>n1>>n2;
+
+    cout<<"\n Before swapping: ";
+
+    cout<<"\n num1= "<<n1<<"\n num2= "<<n2;
+
+    s=n1;
+
+    n1=n2;
+
+    n2=s;
+
+    cout<<"\n After swapping: ";
+
+    cout<<"\n num1= "<<n1<<"\n num2= "<<n2;
+
+    return 0;
+
+}
